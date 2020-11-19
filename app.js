aaee623cd9ff72892a17892233850d972e438f97
AmirArreaza <ajaarreazav@gmail.com>
2020-11-19 22:52:40 +0000

    Sending the response with res.send()

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const http = require("http");
-
 const express = require("express");
 
 const app = express();
@@ -13,6 +12,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log(`In another the middlewware`);
   console.log(`Request from ${req.url}`);
+  res.send('<h1>Express Js</h1>');
 });
 
 const server = http.createServer(app);
