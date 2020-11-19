bd6ada47d8e45a02d53de98a1cb9412878273c9b
AmirArreaza <ajaarreazav@gmail.com>
2020-11-19 22:49:17 +0000

    Added middleware to every request

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require('http');
+const http = require("http");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(`In the middlewware`);
+  console.log(`Request from ${req.url}`);
+  next();//Allows the requrest to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+  console.log(`In another the middlewware`);
+  console.log(`Request from ${req.url}`);
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
