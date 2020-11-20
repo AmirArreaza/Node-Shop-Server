2d9023bd16e9a126c899350d711d43018dc9f637
AmirArreaza <ajaarreazav@gmail.com>
2020-11-20 19:07:19 +0000

    Fixing styling, adding a bit of dynamic data

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,9 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+
+const products = [];
 
 // GET /admin/add-product
 
@@ -16,8 +18,10 @@ router.get("/add-product", (req, res, next) => {
 // POST /admin/add-product
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
+  console.log(`Products ${products}`);
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,12 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get("/", (req, res, next) => {
   console.log(`In another the middlewware`);
   console.log(`Request from ${req.url}`);
+  console.log(adminData.products);
   res.sendFile(path.join(__dirname, '../', "views", "shop.html"));
 });
 
