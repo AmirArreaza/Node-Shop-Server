f07d7d3500b30506ea9725cac6f5abc72e84d880
AmirArreaza <ajaarreazav@gmail.com>
2020-11-20 13:38:50 +0000

    Body-Parser package added to extract the contents of the body

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/', (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
   console.log(`This always runs`);
   console.log(`Request from ${req.url}`);
   next();
 });
-app.use('/product', (req, res, next) => {
-  console.log(`In the Product middlewware`);
+
+app.use("/add-product", (req, res, next) => {
+  console.log(`In the Add-Product middlewware`);
   console.log(`Request from ${req.url}`);
-  res.send('<h1>Express Js -Product-</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add</button></input></form>'
+  );
 });
 
-app.use('/', (req, res, next) => {
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   console.log(`In another the middlewware`);
   console.log(`Request from ${req.url}`);
-  res.send('<h1>Express Js</h1>');
+  res.send("<h1>Express Js</h1>");
 });
 
 app.listen(3000);
