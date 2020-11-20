4c9224a70007c1742bc7a580a9d62ca888892f92
AmirArreaza <ajaarreazav@gmail.com>
2020-11-20 13:41:38 +0000

    Handling /product as a POST request only

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
+
 
 app.use("/", (req, res, next) => {
   console.log(`In another the middlewware`);
