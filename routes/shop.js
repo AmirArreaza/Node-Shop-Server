ba26b955e95dc40ac29057e0a3397fca0ee5f9df
AmirArreaza <ajaarreazav@gmail.com>
2020-11-27 14:44:53 +0000

    Added Handlebars template engine

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,12 @@ router.get("/", (req, res, next) => {
   console.log(`In another the middlewware`);
   console.log(`Request from ${req.url}`);
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
