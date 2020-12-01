5542049354a958b72c9853beb5b5cd8aeb9a2dbb
AmirArreaza <ajaarreazav@gmail.com>
2020-12-01 14:15:10 +0000

    Converging the Add view into a sole Edit view

--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,10 @@ const router = express.Router();
 const adminController = require("../controllers/admin");
 // GET /admin/add-product
 router.get("/add-product", adminController.getAddProduct);
-
 // POST /admin/add-product
 router.post("/add-product", adminController.postAddProduct);
+// GET /admin/edit-product
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
 // GET /admin/products
 router.get("/products", adminController.getProducts);
