89e320189db5c8d96dfce7e3ac8574dfbd933c30
AmirArreaza <ajaarreazav@gmail.com>
2020-12-03 15:24:01 +0000

    SQL db configuration added

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
+const db = require('./util/database');
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
