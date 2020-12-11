0936c36951ba62591b32bbc23cab8ef5d16db606
AmirArreaza <ajaarreazav@gmail.com>
2020-12-11 12:15:45 +0000

    Adding user id ot the product creation

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const newProduct = new Product(title, price, description, imageUrl);
+  const userId = req.user._id;
+  const newProduct = new Product(title, price, description, imageUrl, null, userId);
   newProduct
     .save()
     .then((result) => {
