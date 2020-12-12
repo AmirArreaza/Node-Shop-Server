e780edc23e578eceecf157c4bcd8f5e1beb194b3
AmirArreaza <ajaarreazav@gmail.com>
2020-12-12 19:16:11 +0000

    Using mongoose to products findById

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,7 +19,6 @@ exports.getProducts = (req, res, next) => {
 exports.getProductById = (req, res, next) => {
   const prodId = req.params.productId;
   console.log(`Looking for ${prodId} Product`);
-  //Product.findByPk({
   Product.findById(prodId)
     .then((product) => {
       res.render("shop/product-detail", {
--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,8 @@ const shopController = require("../controllers/shop");
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
-/*
 router.get("/products/:productId", shopController.getProductById);
+/*
 router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.addToCart);
 router.post("/cart-delete-item", shopController.postCartDeleteItem);
